a77090bb9840a3f81778274eafb4cbeed26a6089
Mark Bendarczyk <mark@slytechs.com>
2024-12-25 16:29:07 -0500

    Updated for refactored platform-api module

--- src/main/java/com/slytechs/jnet/jnetpcap/example/PacketType2Descriptor.java
+++ src/main/java/com/slytechs/jnet/jnetpcap/example/PacketType2Descriptor.java
@@ -23,7 +23,7 @@ import java.io.IOException;
 import org.jnetpcap.PcapException;
 
 import com.slytechs.jnet.jnetpcap.api.NetPcap;
-import com.slytechs.jnet.platform.api.time.Timestamp;
+import com.slytechs.jnet.platform.api.util.time.Timestamp;
 import com.slytechs.jnet.protocol.api.packet.Packet;
 
 /**
